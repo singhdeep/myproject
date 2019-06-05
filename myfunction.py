20b654ea92d50dc8a5d84b327226568c48a5a49f
singhdeep <singhdeep@users.noreply.github.com>
2019-06-05 14:42:01 +0530

    Delete myfunction.py

--- myfunction.py
+++
@@ -1,6 +0,0 @@
-class Person():
- def __init__(self,name,message):
-  self.name = name
-  self.message = message
-p1 = Person("deepak","humanity should be above all religion")
-print(p1.name,p1.message)
